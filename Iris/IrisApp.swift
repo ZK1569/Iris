d0c1b3f39f7a2f9abf69daf24d85266a6791d140
Cristian URSU <cristian.ursu.2001@gmail.com>
2024-03-30 18:17:13 +0100

    first commit

---
+++ Iris/IrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  IrisApp.swift
+//  Iris
+//
+//  Created by Cristian Ursu on 30/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct IrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
